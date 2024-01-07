088f62b91a95fb52f758911fcc399f24fab71867
Jenny <jjp134@georgetown.edu>
2024-01-07 11:55:07 -0500

    make migrations

--- ArabWorldMap/settings.py
+++ ArabWorldMap/settings.py
@@ -89,20 +89,20 @@ WSGI_APPLICATION = 'ArabWorldMap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgres://arab_world_map_user:758ibigxVDN1hMUWBNIVOd0F9EaXF9tz@dpg-cmam6fol5elc73em29d0-a/arab_world_map',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Feel free to alter this value to suit your needs.
+#         default='postgres://arab_world_map_user:758ibigxVDN1hMUWBNIVOd0F9EaXF9tz@dpg-cmam6fol5elc73em29d0-a/arab_world_map',
+#         conn_max_age=600
+#     )
+# }
 
 
 # Password validation
