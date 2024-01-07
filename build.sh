c71e835a5a158fc706b1cea264c401fa48e01e43
ojenny001 <76223794+ojenny001@users.noreply.github.com>
2024-01-07 12:46:21 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,6 @@ then
 fi
 
 python manage.py collectstatic --no-input
-python manage.py migrate --fake quizzes zero
-python manage.py migrate --fake results zero
 
 python manage.py migrate quizzes
 python manage.py migrate results
