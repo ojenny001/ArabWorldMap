d07f957251cd732efe8c76631141ac934ac1ae32
Jenny <jjp134@georgetown.edu>
2024-01-07 13:50:40 -0500

    changed database

--- ArabWorldMap/settings.py
+++ ArabWorldMap/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'ArabWorldMap.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgres://arab_world_map_user:758ibigxVDN1hMUWBNIVOd0F9EaXF9tz@dpg-cmam6fol5elc73em29d0-a/arab_world_map',
+        default='postgres://arab_world_map_tnrq_user:675vKdnbyp6dxF9MMV99iXMNQeOlZ1dR@dpg-cmdf3hn109ks7397q370-a/arab_world_map_tnrq',
         conn_max_age=600
     )
 }
