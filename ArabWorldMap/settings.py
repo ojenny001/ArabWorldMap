4cb9e932a66f8c5d36e6166be76028a42baff754
Jenny <jjp134@georgetown.edu>
2024-01-07 20:30:30 -0500

    changed database

--- ArabWorldMap/settings.py
+++ ArabWorldMap/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import psycopg2
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
